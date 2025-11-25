221af8816b5330069669240dd92a74ac6d682a88
Caio Renan Pereira Eloi <caiozin.r1227@gmail.com>
2025-11-25 11:33:29 -0300

    Delete Condições directory

--- Condições/cond2.js
+++
@@ -1,8 +0,0 @@
-var idade = 15
-if (idade < 16) {
-    console.log('Não vota')
-} else if (idade >= 16 && idade < 18) {
-    console.log('Voto opcional')
-} else {
-    console.log('Voto obrigatório')
-}
